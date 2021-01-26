4e6a29dfc48d1f345a586bfd17f6cab9b6a601a9
giizeem <giizeeem61@gmail.com>
2021-01-26 22:49:38 +0200

    Add project files.

---
+++ Oop2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Gizem";
+            musteri1.Soyadi = "Kaya";
+            musteri1.TcNo = "121212";
+            musteri1.Id = 1;
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "1212";
+            musteri2.SirketAdi = "Ada";
+            musteri2.VergiNo = "121212";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+
+        }
+    }
+}
